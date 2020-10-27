d10bf6c8e9c7fc6df7a048f767114beab6ccd56c
Tomas O Conghaile <G00366058@gmit.ie>
2020-10-27 14:01:58 +0000

    Lab Upload

--- src/components/create.js
+++ src/components/create.js
@@ -1,13 +1,81 @@
-import React from 'react';
+import React from 'react';  
 
 export class Create extends React.Component{
 
+    constructor(){
+        super();
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeMovieYear = this.onChangeMovieYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        this.state = {
+            Title:'',
+            Year:'',
+            Poster:''
+        }
+    }
+
+    onChangeMovieName(e){
+        this.setState({ Title: e.target.value})
+    }
+
+    onChangeMovieYear(e){
+        this.setState({
+            Year: e.target.value
+        })
+    }
+
+    onChangePoster(e){
+        this.setState({
+            Poster: e.target.value
+        })
+    }
+
+    onSubmit(){
+        alert("Movie added Title"
+        +this.state.Title + ' Year: '
+        +this.state.Year + ' Poster: '
+        +this.state.Poster);
+    }
     render(){
         return(
 <div>
     <h1>This is the create  component</h1>
-</div>
 
-        );
+    <form onSubmit={this.onSubmit}>
+
+        <div className="form-group">
+            <label>please add movie title</label>
+            <input type = 'text'
+            className="form-control"
+            value={this.state.Title}
+            onChange={this.onChangeMovieName}></input>
+        </div>
+
+        <div className="form-group">
+        <label>please add movie year</label>
+        <input type = 'text'
+        className="form-control"
+        value={this.state.Year}
+        onChange={this.onChangeMovieYear}></input>
+
+        <div className="form-group">
+        <label>please add movie Poster</label>
+        <input type = 'text'
+        className="form-control"
+        value={this.state.Poster}
+        onChange={this.onChangePoster}></input>
+   </div>
+   <div className ="form-group">
+       <input type='submit'
+       value='Add Movie'
+       className='btn btn-primary'></input>
+   </div>
+   </div>
+</form>
+   
+   </div>
+        )
     }
 }
--- src/components/read.js
+++ src/components/read.js
@@ -1,40 +1,31 @@
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
 
 export class Read extends React.Component{
 
     state = {
-        movies: [  
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
+        movies: []
     };
-    render(){
-        return(
-<div>
-    <h1>This is the read  component</h1>
-        <Movies movies={this.state.movies}></Movies>
-</div>
-
+    componentDidMount() {
+        axios.get('https:jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+        .then(response =>{
+            this.setState({movies: response.data.Search})
+        })
+        .catch(
+            (error)=>{
+                console.log(error);
+            }
         );
     }
-}
+        render(){
+            return(
+        
+        <div>
+    <h1>This is the read  component</h1>
+        <Movies movies={this.state.movies}></Movies>
+        
+</div>
+            )
+            }
+        }
